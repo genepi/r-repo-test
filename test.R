3fcb504b904afcabdb4c232600ed2b3e760291c8
seppinho <sebastian.schoenherr@i-med.ac.at>
2022-05-03 15:28:35 +0200

    Important change in test.R

--- test.R
+++ test.R
@@ -2,4 +2,5 @@ library(ggplot2)
 library(here)
 df <- read.table(here("data", "food.csv"), header=TRUE, sep=",", dec = ".")
 plot(df$Data.Carbohydrate, df$Data.Choline)
+plot(df$Data.Carbohydrate, df$Data.Choline)
 
